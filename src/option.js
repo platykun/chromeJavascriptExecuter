ae5c1d9f8cd90a37379d52e6ea85710ecfbeb065
platykun <nagonago561@yahoo.co.jp>
2019-04-28 18:51:09 +0900

    JSの編集削除機能追加

--- src/option.js
+++ src/option.js
@@ -1,4 +1,6 @@
-// 登録ボタン押下イベントを取得してjsを実行するイベントを呼ぶ
+/**
+ * JSを登録する
+ */
 function submitJs() {
   let localStorageItem = localStorage.getItem('jsList');
   let data = localStorageItem === null ? [] : JSON.parse(localStorageItem);
@@ -8,25 +10,92 @@ function submitJs() {
 
   data.push({js: jsValue, title: titleValue});
   localStorage.setItem('jsList', JSON.stringify(data));
+  location.reload();
 }
-document.querySelector('.submit').addEventListener('click', submitJs);
 
-// 画面初期表示時、localStorage情報から登録済みjavascript情報を取得する
+/**
+ * JSを更新する
+ *
+ * @param index 更新対象のJSのindex番号
+ * @param title タイトル
+ * @param js javascript
+ */
+function updateJs(index, title, js) {
+  let updateTargetJs = localStorage.getItem('jsList');
+  let data = JSON.parse(updateTargetJs);
+  data[index].js = js;
+  data[index].title = title;
+
+  localStorage.setItem('jsList', JSON.stringify(data));
+  location.reload();
+}
+
+/**
+ * JSを削除する
+ * @param index 削除対象JSのindex番号
+ *
+ * lovalStorage以外の更新処理はどうするかは要検討
+ */
+function deleteJs(index) {
+  let deleteTargetJs = localStorage.getItem('jsList');
+  let data = JSON.parse(deleteTargetJs);
+  data.splice(index, 1);
+
+  localStorage.setItem('jsList', JSON.stringify(data));
+  location.reload();
+}
+
+/**
+ * 画面初期表示時、localStorage情報から登録済みjavascript情報を取得する
+ */
 let localStorageItem = localStorage.getItem('jsList');
 let data = localStorageItem === null ? [] : JSON.parse(localStorageItem);
 for (let i = 0; i < data.length; i++) {
   let title = data[i]["title"];
-  $('#registerList').append('<tr><th scope="row">'+ title +'</th></tr>');
+  const js = data[i]["js"];
+  const dataTarget = "dataTarget" + i;
+
+  $('#registerList').append(
+    '<tr>' +
+    '<th scope="row" data-toggle="modal" data-target="#' + dataTarget + '">' + title + '</th>' +
+    '</tr>');
+  $('#modalList').append(
+    '<div class="modal fade" id="' + dataTarget + '" tabindex="-1" role="dialog" aria-labelledby="label1" aria-hidden="true">' +
+    '<div class="modal-dialog" role="document">' +
+    '<div class="modal-content">' +
+    '<div class="modal-header">' +
+    '<h5 class="modal-title" id="label1">編集</h5>' +
+    '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+    '<span aria-hidden="true">&times;</span>' +
+    '</button>' +
+    '</div>' +
+    '<div class="modal-body">' +
+    '<div class="form-group">' +
+    '<label>Title</label>' +
+    '<input class="form-control" id=' + dataTarget + 'title name="title" value="' + title + '"/>' +
+    '<label>JavaScript</label>' +
+    '<textarea class="form-control" id=' + dataTarget + 'js name="js" rows="3">' + js + '</textarea>' +
+    '</div>' +
+    '</div>' +
+    '<div class="modal-footer">' +
+    '<button type="button" class="btn btn-danger" id=' + dataTarget + 'delete data-dismiss="modal">削除</button>' +
+    '<button type="button" class="btn btn-primary" id=' + dataTarget + 'update >更新</button>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>'
+  );
+
+  document.querySelector('#' + dataTarget + 'update')
+    .addEventListener('click', function () {
+      let updatedTitle = $("#dataTarget" + i + "title")[0].value;
+      const updatedJs = $("#dataTarget" + i + "js")[0].value;
+      updateJs(i, updatedTitle, updatedJs);
+    });
+  document.querySelector('#' + dataTarget + 'delete')
+    .addEventListener('click', function () {
+      deleteJs(i);
+    });
 }
 
-// 登録ボタン押下イベントを取得して登録済みjavascriptの情報を更新する
-function updateRegisterList() {
-  let localStorageItem = localStorage.getItem('jsList');
-  let data = localStorageItem === null ? [] : JSON.parse(localStorageItem);
-  $('#registerList').empty();
-  for (let i = 0; i < data.length; i++) {
-    let title = data[i]["title"];
-    $('#registerList').append('<tr><th scope="row">'+ title +'</th></tr>');
-  }
-}
-document.querySelector('.submit').addEventListener('click', updateRegisterList);
+document.querySelector('#submit').addEventListener('click', submitJs);
